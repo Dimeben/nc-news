9a78216b798a4b5bc7147a0bdb593194f5a889ee
Ben McCarthy <ben.mccarthy92@gmail.com>
2024-08-26 10:25:02 +0100

    Completes task 22 - ADVANCED: POST /api/topics

--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -1,9 +1,26 @@
-const { selectTopics } = require("../models/topics-model");
+const { selectTopics, createTopic } = require("../models/topics-model");
+
+const isValidString = (value) =>
+  typeof value === "string" && value.trim().length > 0;
 
 exports.getTopics = (req, res, next) => {
   selectTopics()
     .then((topics) => {
       res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.postTopic = (req, res, next) => {
+  const { slug, description } = req.body;
+  if (!isValidString(slug) || !isValidString(description)) {
+    return next({ status: 400, msg: "Bad request" });
+  }
+  createTopic(slug, description)
+    .then((topic) => {
+      res.status(201).send({ topic });
     })
     .catch((err) => {
       next(err);
