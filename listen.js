ca04d1a7af85d7c6efbca34da95d9ced45697c46
Ben McCarthy <ben.mccarthy92@gmail.com>
2024-08-29 09:21:23 +0100

    Update the dependencies

--- listen.js
+++ listen.js
@@ -1,6 +1,10 @@
 const app = require("./app");
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Listening on ${PORT}`);
+  }
 });
